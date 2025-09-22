23f51984da3b0b675f0eefc2f5aae0b781840d7f
LuisPereira05 <luispereira.sl014@academico.ifsul.edu.br>
2025-09-22 20:15:54 -0300

    init

---
+++ src/main/java/com/web2/dto/CursoDTO.java
@@ -0,0 +1,17 @@
+package com.web2.dto;
+
+import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record CursoDTO(
+    @NotBlank String nome,
+    @NotBlank String descricao,
+    LocalDateTime data_inicio,
+    LocalDateTime data_final,
+    String img,
+    @NotNull Long professor_ID,
+    @NotNull Long categoria_ID
+) {}
+
---
+++ src/main/java/com/web2/repository/CategoriaRepository.java
@@ -0,0 +1,16 @@
+package com.web2.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.web2.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+	List<Categoria> findCategoriaByNomeLike(String nome);
+	List<Categoria> findAll();
+	Optional<Categoria> findById(Long categoriaId);
+}
